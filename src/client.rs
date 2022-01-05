dba031b76ac3678765cbb8f9328288174a66d86f
Kyle Ladd <kyle@ladd.io>
2022-01-05 09:30:26 -0500

    Share client reference between rpc types

--- src/client.rs
+++ src/client.rs
@@ -3,18 +3,27 @@ use prost::Message;
 
 use std::io::{Read, Write};
 use std::net::{SocketAddrV4, TcpStream};
+use std::sync::Mutex;
 
 use crate::schema;
 
 pub struct Client {
-    rpc: TcpStream,
+    rpc: Mutex<TcpStream>,
 }
 
 // TODO(kladd):
 impl Client {
-    fn new(name: &str, ip_addr: &str, rpc_port: u16, stream_port: u16) -> Self {
-        let mut rpc = TcpStream::connect(SocketAddrV4::new(ip_addr.parse().unwrap(), rpc_port))
-            .expect("binding address");
+    pub fn new(
+        name: &str,
+        ip_addr: &str,
+        rpc_port: u16,
+        stream_port: u16,
+    ) -> Self {
+        let mut rpc = TcpStream::connect(SocketAddrV4::new(
+            ip_addr.parse().unwrap(),
+            rpc_port,
+        ))
+        .expect("binding address");
 
         // TODO(kladd): 512?
         let mut buf = BytesMut::with_capacity(512);
@@ -35,35 +44,37 @@ impl Client {
         // TODO(kladd): fixed buffer size.
         let mut res_buf = vec![0u8; 28];
         let n = rpc.read(&mut res_buf[..]).expect("reading response");
-        dbg!(&res_buf[..n]);
 
-        let response = schema::ConnectionResponse::decode_length_delimited(&res_buf[..n])
-            .expect("decode response");
-        dbg!(&response);
+        let response =
+            schema::ConnectionResponse::decode_length_delimited(&res_buf[..n])
+                .expect("decode response");
 
-        Self { rpc }
+        Self {
+            rpc: Mutex::new(rpc),
+        }
     }
 
-    pub fn call(&mut self, request: schema::Request) -> schema::Response {
+    pub fn call(&self, request: schema::Request) -> schema::Response {
+        let mut rpc = self.rpc.lock().unwrap();
+
         let mut request_buf = Vec::new();
         request
             .encode_length_delimited(&mut request_buf)
             .expect("encode request");
 
-        dbg!(&request_buf);
-        self.rpc.write(&request_buf).expect("rpc send request");
-        self.rpc.flush().expect("rpc send flush");
+        rpc.write(&request_buf).expect("rpc send request");
+        rpc.flush().expect("rpc send flush");
 
         // TODO(kladd): fixed buffer size.
         let mut response_buf = vec![0u8; 256];
-        let n = self
-            .rpc
+        let n = rpc
             .read(&mut response_buf[..])
             .expect("rpc recieve response");
 
-        dbg!(&response_buf[..n]);
+        drop(rpc);
 
-        schema::Response::decode_length_delimited(&response_buf[..n]).expect("decode response")
+        schema::Response::decode_length_delimited(&response_buf[..n])
+            .expect("decode response")
     }
 
     pub fn proc_call(
@@ -77,39 +88,5 @@ impl Client {
         proc.arguments = args;
 
         proc
-    }
-}
-
-mod services {
-    use prost::Message;
-
-    use crate::schema;
-
-    use super::Client;
-
-    pub struct KRPC;
-
-    impl KRPC {
-        pub fn get_status(client: &mut Client) -> schema::Status {
-            let request = schema::Request::from(Client::proc_call("KRPC", "GetStatus", Vec::new()));
-
-            let response = client.call(request);
-
-            schema::Status::decode(&response.results[0].value[..]).expect("decode status")
-        }
-    }
-}
-
-#[cfg(test)]
-mod test {
-
-    use super::*;
-
-    #[test]
-    fn test() {
-        let mut client = Client::new("rpc test", "127.0.0.1", 50000, 50001);
-        let status = services::KRPC::get_status(&mut client);
-
-        dbg!(status);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,102 @@ mod schema {
             }
         }
     }
+
+    impl From<Response> for f64 {
+        fn from(r: Response) -> Self {
+            let v = r.results[0].value.to_owned();
+            f64::from_le_bytes(v.try_into().expect("expecting f64"))
+        }
+    }
 }
 
 mod client;
+
+mod services {
+    use std::sync::Arc;
+
+    use prost::Message;
+
+    use crate::client::Client;
+    use crate::schema;
+
+    macro_rules! rpc_object {
+        ($name:ident) => {
+            #[derive(Debug)]
+            pub struct $name {
+                id: u64,
+            }
+            impl From<crate::schema::Response> for $name {
+                fn from(response: crate::schema::Response) -> Self {
+                    $name {
+                        id: u64::from(response),
+                    }
+                }
+            }
+        };
+    }
+
+    pub struct KRPC {
+        client: Arc<Client>,
+    }
+
+    impl KRPC {
+        pub fn new(client: Arc<Client>) -> Self {
+            KRPC { client }
+        }
+
+        pub fn get_status(&self) -> schema::Status {
+            let request = schema::Request::from(Client::proc_call(
+                "KRPC",
+                "GetStatus",
+                Vec::new(),
+            ));
+
+            let response = self.client.call(request);
+
+            schema::Status::decode(&response.results[0].value[..])
+                .expect("decode status")
+        }
+    }
+
+    pub struct SpaceCenter {
+        client: Arc<Client>,
+    }
+
+    impl SpaceCenter {
+        pub fn new(client: Arc<Client>) -> Self {
+            SpaceCenter { client }
+        }
+
+        pub fn get_ut(&self) -> f64 {
+            let request = schema::Request::from(Client::proc_call(
+                "SpaceCenter",
+                "get_UT",
+                Vec::new(),
+            ));
+
+            let response = self.client.call(request);
+
+            f64::from(response)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+
+    use crate::client::Client;
+    use crate::services;
+
+    #[test]
+    fn call() {
+        let client =
+            Arc::new(Client::new("rpc test", "127.0.0.1", 50000, 50001));
+
+        let krpc = services::KRPC::new(Arc::clone(&client));
+        let sc = services::SpaceCenter::new(Arc::clone(&client));
+        dbg!(sc.get_ut());
+        dbg!(krpc.get_status());
+    }
+}
