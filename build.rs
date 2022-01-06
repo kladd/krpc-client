ab9846641a90ae3c8a7a60eebff1b04adbb8554a
Kyle Ladd <kyle@ladd.io>
2022-01-06 18:14:24 -0500

    rustfmt

--- build.rs
+++ build.rs
@@ -43,35 +43,35 @@ use convert_case::{Case, Casing};
 // }
 
 fn main() {
-	// println!("cargo:rerun-if-changed=proto/krpc.proto");
-	prost_build::compile_protos(&["proto/krpc.proto"], &["proto/"]).unwrap();
+    // println!("cargo:rerun-if-changed=proto/krpc.proto");
+    prost_build::compile_protos(&["proto/krpc.proto"], &["proto/"]).unwrap();
 
-	// TODO(kladd): scan service_definitions.
-	let sc_file =
-		fs::File::open("service_definitions/KRPC.SpaceCenter.json").unwrap();
+    // TODO(kladd): scan service_definitions.
+    let sc_file =
+        fs::File::open("service_definitions/KRPC.SpaceCenter.json").unwrap();
 
-	let mut out_file = fs::File::create("target/test.txt").unwrap();
+    let mut out_file = fs::File::create("target/test.txt").unwrap();
 
-	let json: serde_json::Value = serde_json::from_reader(sc_file).unwrap();
+    let json: serde_json::Value = serde_json::from_reader(sc_file).unwrap();
 
-	// json.as_object().unwrap()._each(|k| {
-	//     // write!(out_file, "{}", k.to_case(Case::Snake)).unwrap();
-	// });
+    // json.as_object().unwrap()._each(|k| {
+    //     // write!(out_file, "{}", k.to_case(Case::Snake)).unwrap();
+    // });
 
-	json.as_object()
-		.unwrap()
-		.into_iter()
-		.for_each(|(name, def)| {
-			write!(out_file, "mod {}\n", name.to_case(Case::Snake)).unwrap();
-			def.as_object()
-				.unwrap()
-				.get("procedures")
-				.unwrap()
-				.as_object()
-				.unwrap()
-				.into_iter()
-				.for_each(|(proc_name, proc_def)| {
-					write!(out_file, "\tfn {}\n", proc_name).unwrap();
-				})
-		})
+    json.as_object()
+        .unwrap()
+        .into_iter()
+        .for_each(|(name, def)| {
+            write!(out_file, "mod {}\n", name.to_case(Case::Snake)).unwrap();
+            def.as_object()
+                .unwrap()
+                .get("procedures")
+                .unwrap()
+                .as_object()
+                .unwrap()
+                .into_iter()
+                .for_each(|(proc_name, proc_def)| {
+                    write!(out_file, "\tfn {}\n", proc_name).unwrap();
+                })
+        })
 }
--- src/client.rs
+++ src/client.rs
@@ -8,85 +8,85 @@ use std::sync::Mutex;
 use crate::schema;
 
 pub struct Client {
-	rpc: Mutex<TcpStream>,
+    rpc: Mutex<TcpStream>,
 }
 
 // TODO(kladd):
 impl Client {
-	pub fn new(
-		name: &str,
-		ip_addr: &str,
-		rpc_port: u16,
-		stream_port: u16,
-	) -> Self {
-		let mut rpc = TcpStream::connect(SocketAddrV4::new(
-			ip_addr.parse().unwrap(),
-			rpc_port,
-		))
-		.expect("binding address");
+    pub fn new(
+        name: &str,
+        ip_addr: &str,
+        rpc_port: u16,
+        stream_port: u16,
+    ) -> Self {
+        let mut rpc = TcpStream::connect(SocketAddrV4::new(
+            ip_addr.parse().unwrap(),
+            rpc_port,
+        ))
+        .expect("binding address");
 
-		// TODO(kladd): 512?
-		let mut buf = BytesMut::with_capacity(512);
+        // TODO(kladd): 512?
+        let mut buf = BytesMut::with_capacity(512);
 
-		// Send connection request.
-		let mut request = schema::ConnectionRequest::default();
-		request.set_type(schema::connection_request::Type::Rpc);
-		request.client_name = String::from(name);
+        // Send connection request.
+        let mut request = schema::ConnectionRequest::default();
+        request.set_type(schema::connection_request::Type::Rpc);
+        request.client_name = String::from(name);
 
-		request
-			.encode_length_delimited(&mut buf)
-			.expect("encoding request");
+        request
+            .encode_length_delimited(&mut buf)
+            .expect("encoding request");
 
-		rpc.write(&buf.split()).expect("sending request");
-		rpc.flush().unwrap();
+        rpc.write(&buf.split()).expect("sending request");
+        rpc.flush().unwrap();
 
-		// Read response.
-		// TODO(kladd): fixed buffer size.
-		let mut res_buf = vec![0u8; 28];
-		let n = rpc.read(&mut res_buf[..]).expect("reading response");
+        // Read response.
+        // TODO(kladd): fixed buffer size.
+        let mut res_buf = vec![0u8; 28];
+        let n = rpc.read(&mut res_buf[..]).expect("reading response");
 
-		let response =
-			schema::ConnectionResponse::decode_length_delimited(&res_buf[..n])
-				.expect("decode response");
+        let response =
+            schema::ConnectionResponse::decode_length_delimited(&res_buf[..n])
+                .expect("decode response");
 
-		Self {
-			rpc: Mutex::new(rpc),
-		}
-	}
+        Self {
+            rpc: Mutex::new(rpc),
+        }
+    }
 
-	pub fn call(&self, request: schema::Request) -> schema::Response {
-		let mut rpc = self.rpc.lock().unwrap();
+    pub fn call(&self, request: schema::Request) -> schema::Response {
+        let mut rpc = self.rpc.lock().unwrap();
 
-		let mut request_buf = Vec::new();
-		request
-			.encode_length_delimited(&mut request_buf)
-			.expect("encode request");
+        let mut request_buf = Vec::new();
+        request
+            .encode_length_delimited(&mut request_buf)
+            .expect("encode request");
 
-		rpc.write(&request_buf).expect("rpc send request");
-		rpc.flush().expect("rpc send flush");
+        rpc.write(&request_buf).expect("rpc send request");
+        rpc.flush().expect("rpc send flush");
 
-		// TODO(kladd): fixed buffer size.
-		let mut response_buf = vec![0u8; 256];
-		let n = rpc
-			.read(&mut response_buf[..])
-			.expect("rpc recieve response");
+        // TODO(kladd): fixed buffer size.
+        let mut response_buf = vec![0u8; 256];
+        let n = rpc
+            .read(&mut response_buf[..])
+            .expect("rpc recieve response");
 
-		drop(rpc);
+        drop(rpc);
 
-		schema::Response::decode_length_delimited(&response_buf[..n])
-			.expect("decode response")
-	}
+        schema::Response::decode_length_delimited(&response_buf[..n])
+            .expect("decode response")
+    }
 
-	pub fn proc_call(
-		service: &str,
-		procedure: &str,
-		args: Vec<schema::Argument>,
-	) -> schema::ProcedureCall {
-		let mut proc = schema::ProcedureCall::default();
-		proc.service = String::from(service);
-		proc.procedure = String::from(procedure);
-		proc.arguments = args;
+    pub fn proc_call(
+        service: &str,
+        procedure: &str,
+        args: Vec<schema::Argument>,
+    ) -> schema::ProcedureCall {
+        let mut proc = schema::ProcedureCall::default();
+        proc.service = String::from(service);
+        proc.procedure = String::from(procedure);
+        proc.arguments = args;
 
-		proc
-	}
+        proc
+    }
 }
