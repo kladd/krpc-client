c44748054fb7dd7d465ce92aa89a1c937206a2ed
Kyle Ladd <kyle@ladd.io>
2022-01-06 14:37:30 -0500

    Prototype complex return type rpc (and rustfmt)

--- src/lib.rs
+++ src/lib.rs
@@ -1,246 +1,312 @@
 mod schema {
-    include!(concat!(env!("OUT_DIR"), "/krpc.schema.rs"));
+	include!(concat!(env!("OUT_DIR"), "/krpc.schema.rs"));
 
-    use protobuf::types::ProtobufType;
+	use std::{collections::HashMap, hash::Hash};
 
-    pub trait ToArgument {
-        fn to_argument(&self, pos: u32) -> Argument;
-    }
+	use prost::Message;
+	use protobuf::types::ProtobufType;
 
-    macro_rules! from_response_numeric {
-        ($name:ident) => {
-            impl From<Response> for $name {
-                fn from(r: Response) -> Self {
-                    $name::from_le_bytes(
-                        r.results[0]
-                            .value
-                            .to_owned()
-                            .try_into()
-                            .expect(concat!("expecting ", stringify!($name))),
-                    )
-                }
-            }
-        };
-    }
-
-    macro_rules! rpc_object {
-        ($name:ident) => {
-            #[derive(Debug)]
-            pub struct $name {
-                pub id: u64,
-            }
-            impl From<crate::schema::Response> for $name {
-                fn from(response: crate::schema::Response) -> Self {
-                    $name {
-                        id: u64::from(response),
-                    }
-                }
-            }
-        };
-    }
-
-    macro_rules! rpc_enum {
-	($name:ident, [$($value:ident),+$(,)?]) => {
-	    #[derive(Debug, ::num_derive::FromPrimitive)]
-	    pub enum $name {$(
-		$value,
-	    )+}
-
-	    impl From<crate::schema::Response> for $name {
-		fn from(response: crate::schema::Response) -> Self {
-		    ::num_traits::FromPrimitive::from_u8(u8::from(response))
-			.expect("invalid enum value")
-		}
-	    }
+	pub trait ToArgument {
+		fn to_argument(&self, pos: u32) -> Argument;
 	}
-    }
 
-    macro_rules! to_argument {
-        ($t:ty, $fname:ident) => {
-            impl ToArgument for $t {
-                fn to_argument(&self, pos: u32) -> Argument {
-                    let mut buf: Vec<u8> = Vec::new();
-                    {
-                        let mut outstream =
-                            protobuf::CodedOutputStream::new(&mut buf);
-                        outstream.$fname(*self).unwrap();
-                        outstream.flush().unwrap();
-                    }
+	pub trait DecodeUntagged {
+		fn decode_untagged(buf: &Vec<u8>) -> Self;
+	}
 
-                    Argument {
-                        position: pos,
-                        value: buf,
-                    }
-                }
-            }
-        };
-    }
+	// TODO: if String is the only type that doesn't need to be dereferenced, then implement ToArgument for String and get rid of one of these macros.
+	macro_rules! to_argument_deref {
+		($t:ty, $fname:ident) => {
+			impl ToArgument for $t {
+				fn to_argument(&self, pos: u32) -> Argument {
+					let mut buf: Vec<u8> = Vec::new();
+					{
+						let mut outstream =
+							protobuf::CodedOutputStream::new(&mut buf);
+						outstream.$fname(*self).unwrap();
+						outstream.flush().unwrap();
+					}
 
-    macro_rules! from_response {
-        ($to:ty, $proto:ident) => {
-            impl From<Response> for $to {
-                fn from(response: Response) -> Self {
-                    protobuf::types::$proto::read(
-                        &mut protobuf::CodedInputStream::from_bytes(
-                            &response.results[0].value,
-                        ),
-                    )
-                    .unwrap()
-                }
-            }
-        };
-    }
+					Argument {
+						position: pos,
+						value: buf,
+					}
+				}
+			}
+		};
+	}
 
-    impl From<ProcedureCall> for Request {
-        fn from(proc_call: ProcedureCall) -> Self {
-            Request {
-                calls: vec![proc_call],
-            }
-        }
-    }
+	macro_rules! to_argument {
+		($t:ty, $fname:ident) => {
+			impl ToArgument for $t {
+				fn to_argument(&self, pos: u32) -> Argument {
+					let mut buf: Vec<u8> = Vec::new();
+					{
+						let mut outstream =
+							protobuf::CodedOutputStream::new(&mut buf);
+						outstream.$fname(self).unwrap();
+						outstream.flush().unwrap();
+					}
 
-    from_response_numeric!(u8);
-    from_response_numeric!(u32);
-    from_response_numeric!(u64);
-    from_response_numeric!(i32);
-    from_response_numeric!(i64);
-    from_response_numeric!(f32);
-    from_response_numeric!(f64);
-    from_response!(String, ProtobufTypeString);
+					Argument {
+						position: pos,
+						value: buf,
+					}
+				}
+			}
+		};
+	}
 
-    impl ToArgument for String {
-        fn to_argument(&self, pos: u32) -> Argument {
-            let mut buf: Vec<u8> = Vec::new();
-            {
-                let mut outstream = protobuf::CodedOutputStream::new(&mut buf);
-                outstream.write_string_no_tag(&self).unwrap();
-                outstream.flush().unwrap();
-            }
+	macro_rules! from_response_numeric {
+		($name:ident) => {
+			impl From<Response> for $name {
+				fn from(r: Response) -> Self {
+					$name::from_le_bytes(
+						r.results[0]
+							.value
+							.to_owned()
+							.try_into()
+							.expect(concat!("expecting ", stringify!($name))),
+					)
+				}
+			}
+		};
+	}
 
-            Argument {
-                position: pos,
-                value: buf,
-            }
-        }
-    }
+	macro_rules! rpc_object {
+		($name:ident) => {
+			#[derive(Debug, Default)]
+			pub struct $name {
+				pub id: u64,
+			}
 
-    pub(crate) use rpc_enum;
-    pub(crate) use rpc_object;
+			impl From<crate::schema::Response> for $name {
+				fn from(response: crate::schema::Response) -> Self {
+					$name {
+						id: u64::from(response),
+					}
+				}
+			}
+
+			impl DecodeUntagged for $name {
+				fn decode_untagged(buf: &Vec<u8>) -> Self {
+					$name {
+						id: u64::decode_untagged(buf),
+					}
+				}
+			}
+		};
+	}
+
+	macro_rules! rpc_enum {
+		($name:ident, [$($value:ident),+$(,)?]) => {
+			#[derive(Debug, ::num_derive::FromPrimitive)]
+			pub enum $name {$(
+				$value,
+			)+}
+
+			impl From<crate::schema::Response> for $name {
+				fn from(response: crate::schema::Response) -> Self {
+					::num_traits::FromPrimitive::from_u8(u8::from(response))
+					.expect("invalid enum value")
+				}
+			}
+		}
+	}
+
+	macro_rules! from_response {
+		($to:ty, $proto:ident) => {
+			impl From<Response> for $to {
+				fn from(response: Response) -> Self {
+					Self::decode_untagged(&response.results[0].value)
+				}
+			}
+
+			impl DecodeUntagged for $to {
+				fn decode_untagged(b: &Vec<u8>) -> Self {
+					::protobuf::types::$proto::read(
+						&mut ::protobuf::CodedInputStream::from_bytes(b),
+					)
+					.unwrap()
+				}
+			}
+		};
+	}
+
+	impl From<ProcedureCall> for Request {
+		fn from(proc_call: ProcedureCall) -> Self {
+			Request {
+				calls: vec![proc_call],
+			}
+		}
+	}
+
+	impl<K, V> From<Response> for HashMap<K, V>
+	where
+		K: DecodeUntagged + Eq + Hash + Default,
+		V: DecodeUntagged,
+	{
+		fn from(response: Response) -> Self {
+			let mut map: HashMap<K, V> = HashMap::new();
+			let dictionary = Dictionary::from(response);
+			dictionary.entries.into_iter().for_each(|entry| {
+				dbg!(&entry);
+				map.insert(
+					K::decode_untagged(&entry.key),
+					V::decode_untagged(&entry.value),
+				);
+			});
+			map
+		}
+	}
+
+	impl From<Response> for Dictionary {
+		fn from(response: Response) -> Self {
+			Self::decode(&response.results[0].value[..])
+				.expect("unexpected wire type")
+		}
+	}
+
+	from_response!(String, ProtobufTypeString);
+	from_response!(i32, ProtobufTypeInt32);
+	from_response!(i64, ProtobufTypeInt64);
+	from_response!(u32, ProtobufTypeUint32);
+	from_response!(u64, ProtobufTypeUint64);
+	from_response!(f32, ProtobufTypeFloat);
+	from_response!(f64, ProtobufTypeDouble);
+	from_response_numeric!(u8);
+
+	to_argument!(String, write_string_no_tag);
+
+	pub(crate) use rpc_enum;
+	pub(crate) use rpc_object;
 }
 
 mod client;
 
 mod services {
-    use std::sync::Arc;
+	use std::collections::HashMap;
+	use std::sync::Arc;
 
-    use prost::Message;
+	use prost::Message;
 
-    use crate::client::Client;
-    use crate::schema::{self, ToArgument};
+	use crate::client::Client;
+	use crate::schema::{self, DecodeUntagged, ToArgument};
 
-    schema::rpc_object!(Vessel);
-    schema::rpc_enum!(
-        GameMode,
-        [
-            Sandbox,
-            Career,
-            Science,
-            ScienceSandbox,
-            Mission,
-            MissionBuilder,
-            Scenario,
-            ScenarioNonResumable
-        ]
-    );
+	schema::rpc_object!(Vessel);
+	schema::rpc_object!(CelestialBody);
+	schema::rpc_enum!(
+		GameMode,
+		[
+			Sandbox,
+			Career,
+			Science,
+			ScienceSandbox,
+			Mission,
+			MissionBuilder,
+			Scenario,
+			ScenarioNonResumable
+		]
+	);
 
-    pub struct KRPC {
-        client: Arc<Client>,
-    }
+	pub struct KRPC {
+		client: Arc<Client>,
+	}
 
-    impl KRPC {
-        pub fn new(client: Arc<Client>) -> Self {
-            KRPC { client }
-        }
+	impl KRPC {
+		pub fn new(client: Arc<Client>) -> Self {
+			KRPC { client }
+		}
 
-        pub fn get_status(&self) -> schema::Status {
-            let request = schema::Request::from(Client::proc_call(
-                "KRPC",
-                "GetStatus",
-                Vec::new(),
-            ));
+		pub fn get_status(&self) -> schema::Status {
+			let request = schema::Request::from(Client::proc_call(
+				"KRPC",
+				"GetStatus",
+				Vec::new(),
+			));
 
-            let response = self.client.call(request);
+			let response = self.client.call(request);
 
-            schema::Status::decode(&response.results[0].value[..])
-                .expect("decode status")
-        }
-    }
+			schema::Status::decode(&response.results[0].value[..])
+				.expect("decode status")
+		}
+	}
 
-    pub struct SpaceCenter {
-        client: Arc<Client>,
-    }
+	pub struct SpaceCenter {
+		client: Arc<Client>,
+	}
 
-    impl SpaceCenter {
-        pub fn new(client: Arc<Client>) -> Self {
-            SpaceCenter { client }
-        }
+	impl SpaceCenter {
+		pub fn new(client: Arc<Client>) -> Self {
+			SpaceCenter { client }
+		}
 
-        pub fn get_ut(&self) -> f64 {
-            let request = schema::Request::from(Client::proc_call(
-                "SpaceCenter",
-                "get_UT",
-                Vec::new(),
-            ));
+		pub fn get_ut(&self) -> f64 {
+			let request = schema::Request::from(Client::proc_call(
+				"SpaceCenter",
+				"get_UT",
+				Vec::new(),
+			));
 
-            let response = self.client.call(request);
+			let response = self.client.call(request);
 
-            f64::from(response)
-        }
+			f64::from(response)
+		}
 
-        pub fn get_game_mode(&self) -> GameMode {
-            let request = schema::Request::from(Client::proc_call(
-                "SpaceCenter",
-                "get_GameMode",
-                Vec::new(),
-            ));
+		pub fn get_game_mode(&self) -> GameMode {
+			let request = schema::Request::from(Client::proc_call(
+				"SpaceCenter",
+				"get_GameMode",
+				Vec::new(),
+			));
 
-            let response = self.client.call(request);
+			let response = self.client.call(request);
 
-            GameMode::from(response)
-        }
+			GameMode::from(response)
+		}
 
-        pub fn save(&self, name: String) {
-            let request = schema::Request::from(Client::proc_call(
-                "SpaceCenter",
-                "Save",
-                vec![name.to_argument(0)],
-            ));
+		pub fn save(&self, name: String) {
+			let request = schema::Request::from(Client::proc_call(
+				"SpaceCenter",
+				"Save",
+				vec![name.to_argument(0)],
+			));
 
-            let response = self.client.call(request);
+			let response = self.client.call(request);
 
-            dbg!(response);
-        }
-    }
+			dbg!(response);
+		}
+
+		pub fn get_bodies(&self) -> HashMap<String, CelestialBody> {
+			let request = schema::Request::from(Client::proc_call(
+				"SpaceCenter",
+				"get_Bodies",
+				vec![],
+			));
+
+			let response = self.client.call(request);
+
+			HashMap::from(response)
+		}
+	}
 }
 
 #[cfg(test)]
 mod test {
-    use std::sync::Arc;
+	use std::sync::Arc;
 
-    use crate::client::Client;
-    use crate::services;
+	use crate::client::Client;
+	use crate::services;
 
-    #[test]
-    fn call() {
-        let client =
-            Arc::new(Client::new("rpc test", "127.0.0.1", 50000, 50001));
+	#[test]
+	fn call() {
+		let client =
+			Arc::new(Client::new("rpc test", "127.0.0.1", 50000, 50001));
 
-        let krpc = services::KRPC::new(Arc::clone(&client));
-        let sc = services::SpaceCenter::new(Arc::clone(&client));
+		let krpc = services::KRPC::new(Arc::clone(&client));
+		let sc = services::SpaceCenter::new(Arc::clone(&client));
 
-        dbg!(sc.get_ut());
-        dbg!(krpc.get_status());
-        dbg!(sc.get_game_mode());
-        dbg!(sc.save("test_save".into()));
-    }
+		dbg!(sc.save("test_save_two".into()));
+		dbg!(sc.get_bodies());
+	}
 }
