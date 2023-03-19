8722dd9bf302c449fb56bbb0f311f301ee80a85b
Kyle Ladd <kyle@ladd.io>
2023-03-19 16:36:30 -0400

    Remove codegen dependency
    `syn` and `quote` are already transitive dependencies, we can use them
    for code gen.

--- build.rs
+++ build.rs
@@ -4,7 +4,9 @@ use std::{
     io::{Read, Write},
     path::Path,
 };
-mod krpc_codegen;
+mod krpc_build;
+// mod krpc_codegen;
+
 use protobuf_codegen::Customize;
 
 fn main() {
@@ -36,5 +38,10 @@ fn main() {
         .unwrap();
 
     let mut f = File::create(proto_path.join("services.rs")).unwrap();
-    krpc_codegen::build("service_definitions/", &mut f).unwrap();
+    // krpc_codegen::build("service_definitions/", &mut
+    // f).unwrap();
+
+    // let mut x =
+    // File::create(Path::new("services.rs")).unwrap();
+    krpc_build::build("service_definitions/", &mut f).unwrap();
 }
