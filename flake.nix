6f783d7aa044ee9c844badc9cc3d904f76b92ab2
Overloader <overloader@tutanota.com>
2024-09-15 21:09:06 +0200

    Set up nix dev environment

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, rust-overlay, ... }:
+    let
+      system = "x86_64-linux";
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+    in
+    {
+      devShells."${system}".default = pkgs.mkShell {
+        packages = with pkgs; [
+          protobuf
+          rust-bin.stable.latest.default
+        ];
+      };
+    };
+}
